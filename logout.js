d806f99e53ee5e1b98554d3036fe56ddc0914c4a
Mahdi Yusuf <it.mahdi.yusuf@gmail.com>
2019-05-17 16:39:09 +0700

    Update

--- logout.js
+++ logout.js
@@ -1,12 +1,14 @@
 function logout() {
-localStorage.removeItem('id_login')
-localStorage.removeItem('email_login')
-localStorage.removeItem('phoneNumber_login')
-localStorage.removeItem('name_login')
-localStorage.removeItem('isScheduled')
-localStorage.removeItem('-alreadyMakeSchedule')
-
-
-window.location.replace("index.html")
-
+	localStorage.removeItem('id_login')
+	localStorage.removeItem('email_login')
+	localStorage.removeItem('phoneNumber_login')
+	localStorage.removeItem('name_login')
+	localStorage.removeItem('isScheduled')
+	localStorage.removeItem('scheduleID')
+	localStorage.removeItem('sport')
+	localStorage.removeItem('location')
+	localStorage.removeItem('date')
+	localStorage.removeItem('user1name')
+	localStorage.removeItem('user2name')
+	window.location.replace("index.html")
 }
