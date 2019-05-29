ce3eb38dcd4a0ab458b3b998d4e3504a79fed38f
gilangyudharaka <gilang.yudharaka@ui.ac.id>
2019-05-30 01:17:08 +0700

    Updated

--- get-user-schedule.js
+++ get-user-schedule.js
@@ -1,5 +1,5 @@
 function getuser (){
-    getschedule()
+   // getschedule()
 
         $.get("http://localhost:8080/getuser/" + localStorage.getItem('id_login'),function( data ){
             localStorage.setItem('id_login', data.id);
@@ -7,8 +7,12 @@ function getuser (){
             localStorage.setItem('phoneNumber_login', data.phoneNumber);
             localStorage.setItem('email_login', data.email);
             localStorage.setItem('isScheduled',data.isScheduled);
-            localStorage.setItem('scheduleID', data.scheduleID);                
+            localStorage.setItem('scheduleID', data.scheduleID); 
+            getschedule();
         }, "json" );
+
+    
+    
 
         
 }
@@ -16,16 +20,76 @@ function getuser (){
 function getschedule(){
 
 
-        $.get("http://localhost:8080/schedule/" + localStorage.getItem('scheduleID'), function( data ){
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/schedule/'+localStorage.getItem("scheduleID"),
+        success: (data, status, jqXHR) => {
             localStorage.setItem('sport', data.sport);
             localStorage.setItem('location', data.location);
             localStorage.setItem('date', data.date);
-            localStorage.setItem('user1name', data.user1.name);
-            localStorage.setItem('user2name', data.user2.name);
-            localStorage.setItem('user2id', data.user2.id);
-            localStorage.setItem('enemyID', data.user1.enemyID);
-            localStorage.setItem('phoneNumber', data.user2.phoneNumber);
-            
-            
-        }, "json" );
+          
+           
+        },
+        error: (jqXHR, status, errorThrown) => {
+            console.log(jqXHR)
+        },
+        dataType: 'json',
+        timeout: 10000
+    })
+
+
+
+    getuser2id()
 }
+
+
+
+    
+
+function getuser2data(){
+    $.ajax({
+           type: 'GET',
+           url: 'http://localhost:8080/getuser/'+localStorage.getItem("user2id"),
+           success: (data, status, jqXHR) => {
+               //alert("success")
+               localStorage.setItem("user2name",data.name);
+               localStorage.setItem("user2_phoneNumber",data.phoneNumber);
+               window.location.replace("Main.html");
+               
+              
+           },
+           error: (jqXHR, status, errorThrown) => {
+               console.log(jqXHR)
+               
+           },
+           dataType: 'json',
+           timeout: 10000
+       })}
+
+
+       function getuser2id(){
+        $.ajax({
+               type: 'GET',
+               url: 'http://localhost:8080/getuser/'+localStorage.getItem("id_login"),
+               success: (data, status, jqXHR) => {
+                   alert("success")
+                   localStorage.setItem("user2id",data.enemyID);
+                   if(localStorage.getItem("user2id")=="0"){
+                       window.location.replace("Main.html")
+                   }
+                   else{
+                   getuser2data()
+                   }
+                  
+               },
+               error: (jqXHR, status, errorThrown) => {
+                   console.log(jqXHR)
+                   
+               },
+               dataType: 'json',
+               timeout: 10000
+           })
+        
+        
+         
+        }
--- logout.js
+++ logout.js
@@ -1,4 +1,39 @@
+
+
+
+
 function logout() {
 	localStorage.clear();
 	window.location.replace("index.html")
+}
+
+	function main() {
+
+		$.ajax({
+			type: 'GET',
+			url: 'http://localhost:8080/getuser/'+localStorage.getItem("id_login"),
+			success: (data, status, jqXHR) => {
+				console.log(data.id)
+				alert("success")
+				localStorage.setItem('id_login', data.id);
+				localStorage.setItem('name_login', data.name);
+				localStorage.setItem('phoneNumber_login', data.phoneNumber);
+				localStorage.setItem('email_login', data.email);
+				localStorage.setItem('isScheduled',data.isScheduled);
+				localStorage.setItem('scheduleID',data.scheduleID);
+
+
+				window.location.replace("Main.html");
+			},
+			error: (jqXHR, status, errorThrown) => {
+				console.log(jqXHR)
+				alert("failed")
+			},
+			dataType: 'json',
+			timeout: 10000
+		})
+		
+	}
+	function history() {
+			window.location.replace("feedback_history.html")
 }
