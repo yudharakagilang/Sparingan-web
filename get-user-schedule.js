1a55de5e5c86d1cad0e3fb2612ee9a134c5a48d7
Mahdi Yusuf <it.mahdi.yusuf@gmail.com>
2019-05-30 14:02:52 +0700

    Updating Date Format
    Update

--- get-user-schedule.js
+++ get-user-schedule.js
@@ -9,26 +9,17 @@ function getuser (){
             localStorage.setItem('isScheduled',data.isScheduled);
             localStorage.setItem('scheduleID', data.scheduleID); 
             getschedule();
-        }, "json" );
-
-    
-    
-
-        
+        }, "json" );    
 }
 
 function getschedule(){
-
-
     $.ajax({
         type: 'GET',
         url: 'http://localhost:8080/schedule/'+localStorage.getItem("scheduleID"),
         success: (data, status, jqXHR) => {
             localStorage.setItem('sport', data.sport);
             localStorage.setItem('location', data.location);
-            localStorage.setItem('date', data.date);
-          
-           
+            localStorage.setItem('date', GetFormattedDate(data.date));
         },
         error: (jqXHR, status, errorThrown) => {
             console.log(jqXHR)
@@ -37,59 +28,60 @@ function getschedule(){
         timeout: 10000
     })
 
-
-
     getuser2id()
 }
 
-
-
-    
-
 function getuser2data(){
-    $.ajax({
-           type: 'GET',
-           url: 'http://localhost:8080/getuser/'+localStorage.getItem("user2id"),
-           success: (data, status, jqXHR) => {
-               //alert("success")
-               localStorage.setItem("user2name",data.name);
-               localStorage.setItem("user2_phoneNumber",data.phoneNumber);
-               window.location.replace("Main.html");
-               
-              
-           },
-           error: (jqXHR, status, errorThrown) => {
-               console.log(jqXHR)
-               
-           },
-           dataType: 'json',
-           timeout: 10000
-       })}
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost:8080/getuser/'+localStorage.getItem("user2id"),
+    success: (data, status, jqXHR) => {
+      //alert("success")
+      localStorage.setItem("user2name",data.name);
+      localStorage.setItem("user2_phoneNumber",data.phoneNumber);
+      window.location.replace("Main.html");
+    },
+    error: (jqXHR, status, errorThrown) => {
+      console.log(jqXHR)
+    },
+   
+   dataType: 'json',
+   timeout: 10000
+  })
+}
 
+function getuser2id(){
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost:8080/getuser/'+localStorage.getItem("id_login"),
+    success: (data, status, jqXHR) => {
+      alert("success")
+      localStorage.setItem("user2id",data.enemyID);
+      if(localStorage.getItem("user2id")=="0"){
+        window.location.replace("Main.html")
+      }
+      else{
+        getuser2data()
+      }
+    },
+    error: (jqXHR, status, errorThrown) => {
+      console.log(jqXHR)
+    },
+    
+    dataType: 'json',
+    timeout: 10000
+  })
+}
 
-       function getuser2id(){
-        $.ajax({
-               type: 'GET',
-               url: 'http://localhost:8080/getuser/'+localStorage.getItem("id_login"),
-               success: (data, status, jqXHR) => {
-                   alert("success")
-                   localStorage.setItem("user2id",data.enemyID);
-                   if(localStorage.getItem("user2id")=="0"){
-                       window.location.replace("Main.html")
-                   }
-                   else{
-                   getuser2data()
-                   }
-                  
-               },
-               error: (jqXHR, status, errorThrown) => {
-                   console.log(jqXHR)
-                   
-               },
-               dataType: 'json',
-               timeout: 10000
-           })
-        
-        
-         
-        }
+function appendLeadingZeroes(n){
+  if(n <= 9){
+    return "0" + n;
+  }
+  return n
+}
+
+function GetFormattedDate(date) {
+  let d = new Date(date)
+  let dFormat = appendLeadingZeroes(d.getDate()) + "/" + appendLeadingZeroes((d.getMonth() + 1)) + "/" + d.getFullYear()
+  return dFormat;
+}
